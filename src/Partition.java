13801aa68765c3a6c8cc0c4d9959e022e93ade85
walter298 <68366336+walter298@users.noreply.github.com>
2024-12-18 15:53:13 -0800

    added pausable breakpoints

--- src/Partition.java
+++ src/Partition.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.function.Function;
 
 import javafx.animation.ParallelTransition;
+import javafx.animation.SequentialTransition;
 import javafx.scene.paint.Color;
 
 public class Partition {
@@ -19,37 +20,93 @@ public class Partition {
         return last;
     }
 
+    private static final Color PARTITION_POINT_COLOR = Color.GREEN;
+    private static final Color IT_COLOR = Color.BLUE;
+
+    private static void updateStart(AlgorithmStepList steps, ListUI visualList, ArrayList<Integer> integers, int start, int firstLine, int secondLine) {
+        final var startF = start;
+        var line8VarState = new VariableWatchWindow();
+        line8VarState.add("first", VariableWatchWindow.iteratorString(start, integers.get(start)), PARTITION_POINT_COLOR);
+        steps.addStep(() -> { return visualList.setColor(startF, PARTITION_POINT_COLOR); }, line8VarState, firstLine, secondLine);
+    }
+
+    private static void updateStartAndIt(AlgorithmStepList steps, ListUI visualList, ArrayList<Integer> integers, int start, int it, int firstLine, int secondLine) {
+        final var startF = start;
+        final var itF = it;
+        final var firstLineF = firstLine;
+        final var secondLineF = secondLine;
+
+        var watchWindow = new VariableWatchWindow();
+        watchWindow.add("start:", VariableWatchWindow.iteratorString(startF, integers.get(startF)), PARTITION_POINT_COLOR);
+        watchWindow.add("it:", VariableWatchWindow.iteratorString(itF, integers.get(itF)), IT_COLOR);
+        steps.addStep(() -> { 
+            return new SequentialTransition(
+                visualList.setColor(startF, PARTITION_POINT_COLOR),
+                visualList.setColor(itF, IT_COLOR)
+            );
+        }, watchWindow, firstLineF, secondLineF);
+    }
+
+    private static void makeSwapItStartAnimation(AlgorithmStepList steps, ListUI visualList, ArrayList<Integer> integers, int start, int it) {
+        final var startF = start;
+        final var itF = it;
+        steps.addAnimation(() -> { 
+            return new SequentialTransition(
+                visualList.swap(itF, startF),
+                visualList.setColor(itF, PARTITION_POINT_COLOR),
+                visualList.setColor(startF, PARTITION_POINT_COLOR),
+                visualList.setColor(itF, IT_COLOR)
+            );
+        }, 1000, 1000);   
+    }
+
+    private static void incrementStartColor(AlgorithmStepList steps, ListUI visualList, ArrayList<Integer> integers, int start) {
+        final var startF = start;
+        final var inputLen = integers.size();
+        steps.addAnimation(() -> {
+            var transition = new SequentialTransition();
+            transition.getChildren().add(visualList.setColor(startF, Color.BLACK));
+            if (startF + 1 < inputLen) {
+                transition.getChildren().add(visualList.setColor(startF + 1, PARTITION_POINT_COLOR));
+            }
+            return transition;
+        }, 0, 0);
+    }
+
+    private static void blackenOldItValue(AlgorithmStepList steps, ListUI visualList, ArrayList<Integer> integers, int start, int it) {
+        if (it != start + 1 && it > 0) {
+            final var itF = it;
+            steps.addAnimation(() -> { 
+                return visualList.setColor(itF - 1, Color.BLACK);
+            }, 1000, 1000);
+        }
+    }
+
     public static int partition(AlgorithmStepList steps, ListUI visualList, ArrayList<Integer> integers, Function<Integer, Boolean> pred, int first, int last) {
         var start = findFirstNotMatch(integers, pred, first, last);
-        
-        final var PARTITION_POINT_COLOR = Color.GREEN;
+
+        //add line variable state at std::find_if_not around line 8
+        updateStart(steps, visualList, integers, start, 7, 8);
         
         if (start == integers.size()) {
             return start;
         }
 
-        // final int initialStart = start;
-        // steps.addAnimation(() -> { return visualList.setColor(initialStart, PARTITION_POINT_COLOR); });
-
         for (int i = start + 1; i < last; i++) {
-            final var startF = start;
-            steps.addAnimation(() -> { return visualList.setColor(startF, PARTITION_POINT_COLOR); });
+            blackenOldItValue(steps, visualList, integers, start, i);
+            updateStartAndIt(steps, visualList, integers, start, i, 14, 15);
 
             if (pred.apply(integers.get(i))) {
-                //steps.addAnimation(() -> { return visualList.setColor(startF, Color.BLACK); });
-
+                steps.addBreakpoint(15, 16); 
                 Collections.swap(integers, i, start);
-                final int iCopy = i;
 
-                var watchWindow = new VariableWatchWindow();
+                makeSwapItStartAnimation(steps, visualList, integers, start, i);
+                updateStartAndIt(steps, visualList, integers, start, i, 16, 17);
 
-                watchWindow.add("start:", Integer.toString(startF), Color.GREEN);
-                watchWindow.add("it:", Integer.toString(iCopy), Color.BLUE);
-
-                steps.addStep(() -> { return visualList.swap(iCopy, startF); }, watchWindow);
-                //steps.addAnimation(() -> { return visualList.setColor(iCopy, Color.BLACK); });
-                
+                incrementStartColor(steps, visualList, integers, start);
                 start++;
+
+                updateStartAndIt(steps, visualList, integers, start, i, 17, 18);
             }
         }
 
@@ -61,7 +118,6 @@ public class Partition {
     }
 
     private static Function<Integer, Boolean> parsePred(String operator, String strValue) {
-        System.out.println("Parsing " + operator + " " + strValue);
         final int value = Integer.parseInt(strValue);
     
         if (operator.equals("<")) {
@@ -73,8 +129,8 @@ public class Partition {
         }
     }
 
-    public static AlgorithmStepList partition(ListUI visualList, ArrayList<Integer> integers, List<String> args) {
-        var steps = new AlgorithmStepList();
+    public static AlgorithmStepList partition(ListUI visualList, HighlightableCodeArea codeArea, ArrayList<Integer> integers, List<String> args) {
+        var steps = new AlgorithmStepList(codeArea);
         var pred = parsePred(args.get(0), args.get(1));
        
         partition(steps, visualList, integers, pred);
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -7,17 +7,17 @@ import java.util.List;
 import javafx.scene.paint.Color;
 
 public class SelectionSort {
-    public static AlgorithmStepList sort(ListUI visualList, ArrayList<Integer> integers, List<String> args) {
+    public static AlgorithmStepList sort(ListUI visualList, HighlightableCodeArea codeArea, ArrayList<Integer> integers, List<String> args) {
         var sortPred = AlgorithmArgParser.parseSortPred(args.get(0));
 
-        var steps = new AlgorithmStepList();
+        var steps = new AlgorithmStepList(codeArea);
 
         final var IT_COLOR  = Color.GREEN;
         final var MIN_COLOR = Color.BLUE;
 
         for (int i = 0; i < integers.size(); i++) {
             int I = i;
-            steps.addAnimation(() -> { return visualList.setColor(I, IT_COLOR); });
+            steps.addAnimation(() -> { return visualList.setColor(I, IT_COLOR); }, 8, 9);
 
             int min = i; 
             
@@ -28,9 +28,9 @@ public class SelectionSort {
                     final int NEW_MIN = min;
 
                     final var OLD_MIN_COLOR = OLD_MIN == i ? Color.GREEN : Color.BLACK;
-                    steps.addAnimation(() -> { return visualList.setColor(OLD_MIN, OLD_MIN_COLOR); });
+                    steps.addAnimation(() -> { return visualList.setColor(OLD_MIN, OLD_MIN_COLOR); }, 500, 500);
                     
-                    steps.addAnimation(() -> { return visualList.setColor(NEW_MIN, MIN_COLOR); });
+                    steps.addAnimation(() -> { return visualList.setColor(NEW_MIN, MIN_COLOR); }, 500, 500);
                 } 
             }
 
@@ -43,12 +43,12 @@ public class SelectionSort {
                 watchWindow.add("it", VariableWatchWindow.iteratorString(i, integers.get(i)), IT_COLOR);
                 watchWindow.add("min", VariableWatchWindow.iteratorString(min, integers.get(min)), MIN_COLOR);
 
-                steps.addStep(() -> { return visualList.swap(I, MIN); }, watchWindow);
+                steps.addStep(() -> { return visualList.swap(I, MIN); }, watchWindow, 500, 500);
             }
 
             //reset min color and i color
-            steps.addAnimation(() -> { return visualList.setColor(I, Color.BLACK); });
-            steps.addAnimation(() -> { return visualList.setColor(MIN, Color.BLACK); });
+            steps.addAnimation(() -> { return visualList.setColor(I, Color.BLACK); }, 500, 500);
+            steps.addAnimation(() -> { return visualList.setColor(MIN, Color.BLACK); }, 500, 500);
         }
         
         return steps;
